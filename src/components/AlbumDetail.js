739820626c8e69dbb1d6835e505ff9a2c7fb84e5
Priyank Thakkar <priyankthakkar@gmail.com>
2017-11-27 13:29:47 +1100

    Chagne Log: Styling of album detail and card is completed. 2. Linking concept is introduced here to open a URL in the browser. Changes By: Priyank Thakkar

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,25 +1,43 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
-    const { title, artist, thumbnail_image } = album;
-    const { headerContentStyle, thumbailStyle } = styles;
-    
+    const { title, artist, thumbnail_image, image, url } = album;
+    const { 
+            headerContentStyle, 
+            thumbailStyle, 
+            thumbnailContainerStyle, 
+            headerTextStyle, 
+            imageStyle 
+        } = styles;
+
     return (
         <Card>
             <CardSection>
-                <View>
+                <View style={thumbnailContainerStyle}>
                     <Image 
                         style={thumbailStyle} 
                         source={{ uri: thumbnail_image }} 
                     />
                 </View>
                 <View style={headerContentStyle}>
-                    <Text>{title}</Text>
+                    <Text style={headerTextStyle}>{title}</Text>
                     <Text>{artist}</Text>
                 </View>
+            </CardSection>
+            <CardSection>
+                <Image
+                    source={{ uri: image }} 
+                    style={imageStyle}
+                />
+            </CardSection>
+            <CardSection>
+                <Button onPress={() => Linking.openURL(url)}>
+                    Buy Now!
+                </Button>
             </CardSection>
         </Card>
     );
@@ -30,9 +48,23 @@ const styles = {
         flexDirection: 'column',
         justifyContent: 'space-around'
     },
+    headerTextStyle: {
+        fontSize: 18
+    },
     thumbailStyle: {
         height: 50,
         width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
     }
 };
 
